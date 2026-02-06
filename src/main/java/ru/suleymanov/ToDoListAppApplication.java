6244b7008b6a3695fe83b29382f586837e77e713
Solo <rifatsljmnv@yandex.ru>
2026-02-06 16:28:29 +0300

    new project toDoListApp

---
+++ src/main/java/ru/suleymanov/ToDoListAppApplication.java
@@ -0,0 +1,13 @@
+package ru.suleymanov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoListAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoListAppApplication.class, args);
+    }
+
+}
