6e439587dfa0934d3230915c222b5830d32c29dd
Solo <rifatsljmnv@yandex.ru>
2026-02-10 17:16:33 +0300

    Настроено logout User

--- src/main/java/ru/suleymanov/config/SecurityConfig.java
+++ src/main/java/ru/suleymanov/config/SecurityConfig.java
@@ -19,18 +19,29 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login", "/registration", "/error").permitAll()             // url доступны всем пользователям
+                        .requestMatchers("/", "/registration", "/error").permitAll()             // url доступны всем пользователям
                         .requestMatchers("/css/**").permitAll()                                                  // доступ для стилей css,js,image
                         .requestMatchers("/account/**").hasAnyRole(UserRole.USER.name(), UserRole.ADMIN.name())  // url доступны для user
                         .requestMatchers("/admin/**").hasRole("ADMIN")                                           // url доступны для админа
                         .anyRequest().authenticated()
 
                 )
+                // Авторизация и аутентификация
                 .formLogin(form -> form
                         .loginPage("/login")                                                                        // кастомная страница логина
                         .loginProcessingUrl("/login")                                                               // URL, на который форма отправляет логин/пароль
+                        .usernameParameter("email")                                                                 // здесь указан имя поля логина
+                        .passwordParameter("password")                                                              // здесь указан имя поля для пароля
                         .defaultSuccessUrl("/account", true)                               // куда перенаправлять после успешного входа
                         .failureUrl("/error")                                                    // куда перенаправлять при ошибке
+                        .permitAll()
+                )
+                // Выход из аккаунта
+                .logout(logout -> logout
+                        .logoutUrl("/logout")                                                                       // URL, по которому будет выход
+                        .logoutSuccessUrl("/login")                                                                 // куда редирект после выхода
+                        .invalidateHttpSession(true)                                                                // инвалидирует сессию
+                        .deleteCookies("JSESSIONID")                                              // удаляет куки сессии
                         .permitAll()
                 )
                 .logout(LogoutConfigurer::permitAll);
