ebeaa90fd27b16921adb3e6a270cb57e5f285c73
Solo <rifatsljmnv@yandex.ru>
2026-02-10 14:32:47 +0300

    добавлено Spring security config

---
+++ src/main/java/ru/suleymanov/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package ru.suleymanov.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.suleymanov.entity.UserRole;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/login", "/registration", "/error").permitAll()             // url доступны всем пользователям
+                .requestMatchers("/css/**").permitAll()                                                  // доступ для стилей css,js,image
+                .requestMatchers("/account/**").hasAnyRole(UserRole.USER.name(), UserRole.ADMIN.name())  // url доступны для user
+                .requestMatchers("/admin/**").hasRole("ADMIN")                                           // url доступны для админа
+                .anyRequest().authenticated()
+
+            )
+            .formLogin(form -> form
+                .loginPage("/login")                                                                        // кастомная страница логина
+                .loginProcessingUrl("/login")                                                               // URL, на который форма отправляет логин/пароль
+                .defaultSuccessUrl("/account", true)                               // куда перенаправлять после успешного входа
+                .failureUrl("/error")                                                    // куда перенаправлять при ошибке
+                .permitAll()
+            )
+                .logout(logout -> logout.permitAll());
+        return http.build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/ru/suleymanov/entity/UserRole.java
@@ -0,0 +1,6 @@
+package ru.suleymanov.entity;
+
+public enum UserRole {
+    ADMIN,
+    USER;
+}
