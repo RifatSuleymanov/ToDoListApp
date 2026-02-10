6616417978cbc00ef919516248c5c8c2c7555f1b
Solo <rifatsljmnv@yandex.ru>
2026-02-10 17:32:32 +0300

    Авторизация/Идентификация пользователя. add userDetailsService() add toAuthority()

--- src/main/java/ru/suleymanov/config/SecurityConfig.java
+++ src/main/java/ru/suleymanov/config/SecurityConfig.java
@@ -1,25 +1,42 @@
 package ru.suleymanov.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import ru.suleymanov.entity.User;
 import ru.suleymanov.entity.UserRole;
+import ru.suleymanov.repository.UserRepository;
+
+import java.util.Collections;
+import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public SecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/registration", "/error").permitAll()             // url доступны всем пользователям
+                        .requestMatchers("/", "/registration", "/error").permitAll()                         // url доступны всем пользователям
                         .requestMatchers("/css/**").permitAll()                                                  // доступ для стилей css,js,image
                         .requestMatchers("/account/**").hasAnyRole(UserRole.USER.name(), UserRole.ADMIN.name())  // url доступны для user
                         .requestMatchers("/admin/**").hasRole("ADMIN")                                           // url доступны для админа
@@ -46,6 +63,20 @@ public class SecurityConfig {
                 )
                 .logout(LogoutConfigurer::permitAll);
         return http.build();
+    }
+    //Идентификация пользователей
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                User user = userRepository
+                        .findByEmailIgnoreCase(username)
+                        .orElseThrow(() -> new UsernameNotFoundException("User with email = " + username + " not found!"));
+                Set<SimpleGrantedAuthority> roles = Collections.singleton(user.getUserRole().toAuthority());
+                return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), roles);
+            }
+        };
     }
 
     @Bean
--- src/main/java/ru/suleymanov/entity/UserRole.java
+++ src/main/java/ru/suleymanov/entity/UserRole.java
@@ -1,6 +1,14 @@
 package ru.suleymanov.entity;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 public enum UserRole {
     ADMIN,
     USER;
+
+
+    /// ROLE_USER
+    public SimpleGrantedAuthority toAuthority() {
+        return new SimpleGrantedAuthority("ROLE_" + this.name());
+    }
 }
